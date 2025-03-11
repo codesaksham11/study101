729541cf743877fdc134af2e2850fac543dd634d
codesaksham11 <bhattaraij51@gmail.com>
2025-03-11 21:51:09 +0545

    Create authors-english.js

---
+++ flashcards/authors-english.js
@@ -0,0 +1,5 @@
+window.flashcardData = [
+    { back: "author of different stories", front: "../images/IMG_20250311_200053.jpg" },
+    { back: "Authors of poems", front: "../images/IMG_20250311_200451.jpg" },
+    { back: "Author of essays", front: "../images/IMG_20250311_200745.jpg" }
+];
