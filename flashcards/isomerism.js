38ed9e49d06ae8e1de2c3f87c86939151b87703f
codesaksham11 <bhattaraij51@gmail.com>
2025-03-11 23:10:21 +0545

    Update isomerism.js

--- flashcards/isomerism.js
+++ flashcards/isomerism.js
@@ -1,3 +1,3 @@
 window.flashcardData = [
-    { back: "Types of physical isomerism", front: "../images/IMG_20250311_195242.jpg" }
+    { back: "Types of physical isomerism", front: "images/IMG_20250311_195242.jpg" }
 ];
