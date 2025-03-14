e2008efbecc659154fe1086443ded5cb49de2702
codesaksham11 <bhattaraij51@gmail.com>
2025-03-14 18:31:56 +0545

    Update foreign-element.js

--- flashcards/foreign-element.js
+++ flashcards/foreign-element.js
@@ -1,6 +1,6 @@
 window.flashcardData = [
-    { back: "detection of nitrogen as foreign element", front: "/images/IMG_20250311_193207.jpg" },
-    { back: "detection of sulphur and nitrogen as a foreign element", front: "/images/IMG_20250311_193240.jpg" },
-    { back: "detection of chloride as a foreign element", front: "/images/IMG_20250311_193606.jpg" },
-    { back: "detection of iodine as a foreign element", front: "/images/IMG_20250311_194836.jpg" }
+    { back: "detection of nitrogen as foreign element", front: "../images/IMG_20250311_193207.jpg" },
+    { back: "detection of sulphur and nitrogen as a foreign element", front: "../images/IMG_20250311_193240.jpg" },
+    { back: "detection of chloride as a foreign element", front: "../images/IMG_20250311_193606.jpg" },
+    { back: "detection of iodine as a foreign element", front: "../images/IMG_20250311_194836.jpg" }
 ];
